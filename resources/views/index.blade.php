c44b86c9d7aace1659af8579a686159a4999d7a5
issatillie <uarpnn@gmail.com>
2025-01-21 23:01:01 +0100

    Layout and new blade files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $title ?? 'Laravel Auth' }}</title>
-</head>
-<body>
-    
-</body>
-</html>
+<x-layout>
+    <div class="flex items-center justify-center h-screen">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, dignissimos? A saepe quisquam consequatur temporibus tempora, adipisci optio amet quo quas molestias tempore libero. Quis alias nam voluptatem non quam?</p>
+    </div>
+</x-layout>
