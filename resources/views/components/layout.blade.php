1876c2fee23ab5a23df602cf1a846a9861222870
issatillie <uarpnn@gmail.com>
2025-01-21 23:07:26 +0100

    Homepagina

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>{{ $title ?? 'Laravel Auth' }}</title>
 </head>
 <body>
     {{ $slot }}
 
-    <script></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,12 @@
 <x-layout>
     <div class="flex items-center justify-center h-screen">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, dignissimos? A saepe quisquam consequatur temporibus tempora, adipisci optio amet quo quas molestias tempore libero. Quis alias nam voluptatem non quam?</p>
+        <div class="md:max-w-sm space-x-3">
+            <a href="/login" class="transition-all bg-gray-100 hover:bg-gray-300 font-bold py-2 px-4 rounded">
+                Login
+            </a>
+            <a href="/register" class="transition-all bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                Register
+            </a>
+        </div>
     </div>
 </x-layout>
