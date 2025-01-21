57c08fe71e768e5ace437eb7af7a9e6f15c28918
issatillie <uarpnn@gmail.com>
2025-01-21 23:15:21 +0100

    Change index en begin login

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>{{ $title ?? 'Laravel Auth' }}</title>
 </head>
-<body>
+<body class="h-full">
     {{ $slot }}
 
     <script src="{{ asset('js/app.js') }}"></script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,10 @@
 <x-layout>
     <div class="flex items-center justify-center h-screen">
         <div class="md:max-w-sm space-x-3">
-            <a href="/login" class="transition-all bg-gray-100 hover:bg-gray-300 font-bold py-2 px-4 rounded">
-                Login
-            </a>
-            <a href="/register" class="transition-all bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                Register
-            </a>
+            <a href="/login"
+                class="transition-all rounded-md bg-gray-100 px-3 py-1.5 text-sm/6 font-semibold shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-300">Login</a>
+            <a href="/register"
+                class="transition-all rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Register</a>
         </div>
     </div>
 </x-layout>
